048a7fce89adf628197e07646b369846c07b7087
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-10-22 17:20:22 +0200

    todo to find the best algorithm to compare every  possible rotation for maximum output

--- CompareDpsOutput.java
+++ CompareDpsOutput.java
@@ -3,5 +3,7 @@
 public class CompareDpsOutput {
     public void CompareStats() {
         //TODO:: create the option to compare the output depending on the stats of the player   so a player can see what stats are better for higher output
+        //TODO: find the right Algorithm to archiev this
+        
     }
 }
