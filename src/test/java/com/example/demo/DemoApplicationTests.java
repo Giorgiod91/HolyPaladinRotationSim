82a1d822de52a6225c61e35b4f51a6c4aa0738c6
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-01-22 16:13:33 +0100

    adding tests now to check if the dmg is according to the dmg output in game way easier to do test with code than try to test it with the game data only and compare manually

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.model.Paladin;
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testHolyShock() {
+		Paladin paladin = new Paladin();
+		paladin.setMainStat(20000);
+		paladin.setCritChance(20);
+		paladin.setVersatility(20);
+
+
+		int totalDamage = paladin.holyShock();
+
+
+		assertThat(totalDamage).isGreaterThan(0);
 	}
 
 }
